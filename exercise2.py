8afc1a21a0e9cd922f6a7d1c1ecaa76d22780833
AlexStahlman <astahlman17@gmail.com>
2022-01-24 19:11:07 -0500

    2nd commit

--- exercise2.py
+++ exercise2.py
@@ -1,5 +1,9 @@
+import string
+
+
 text = input('Type a phrase ')
 mystring = ''
+// to cycle through the string
 odd = True
 for v in text:
     if odd:
@@ -8,5 +12,6 @@ for v in text:
     else:
         mystring = mystring + v.lower()
     if v != " ":
+        //checking for spaces
         odd = not odd
 print(mystring)
